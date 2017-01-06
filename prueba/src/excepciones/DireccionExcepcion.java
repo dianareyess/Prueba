109df1f1387cb64ab0a2f1d9277fe299c7c86b28
Diana Reyes <diana_reyes2410@outlook.com>
2017-01-05 21:30:23 -0600

    nuevas clases

---
+++ prueba/src/excepciones/DireccionExcepcion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+/**
+ *
+ * @author Diana Reyes Luis
+ */
+
+public class DireccionExcepcion {
+
+}
+
+//cedular, corporacionFiscal, vehicular, hospedaje, intermedio, incorporacion no con intermedio regimen: 
+//persona inscrita a los regimenes.
+//persona, heredar, objetos
+//no tenga dos direcciones fiscales, fiscas. 2 o 3 diferentes.
+//intermedio: no puede estar en incorporacion
