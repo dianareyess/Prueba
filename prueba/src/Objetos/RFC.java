20b925a315411e2368e02e5696ba2dacec285925
Diana Reyes <diana_reyes2410@outlook.com>
2017-01-05 21:43:22 -0600

    ex

---
+++ prueba/src/Objetos/RFC.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+import excepciones.RFCException;
+
+
+/**
+ *
+ * @author Diana Reyes Luis
+ */
+public class RFC {
+    
+    private String rfc;
+    private TipoPersona tipoPersona;
+    
+    public RFC(String rfc, TipoPersona tipo) throws RFCException{
+        this.tipoPersona = tipo;
+        this.rfc = rfc.toUpperCase();
+        if (!this.isValido()){
+            throw new RFCException();
+        }
+    }
+    
+    public boolean isValido(){
+        if (tipoPersona == TipoPersona.FISICA){
+            return rfc.matches("^[A-Z]{4}(\\d{6})((\\D|\\d){3})?$");
+        }else{
+            return rfc.matches("^[A-Z]{3}(\\d{6})((\\D|\\d){3})?$");
+        }
+    }
+
+    
+    @Override
+    public String toString(){
+        return rfc;
+    }
+}
